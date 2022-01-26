493405db8ad33c010249a13cce1db305f07e879c
Linh Lê <ut2405@gmail.com>
2022-01-26 19:40:10 +0200

    edit package.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app        = express();
 const bodyParser = require('body-parser');
 const logger 	   = require('morgan');
 const router 	   = express.Router();
-const port 	   = process.env.PORT || 8080;
+const port 	   = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(logger('dev'));
